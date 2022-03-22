fd82e50d0dc508dfb142e74d6582708d6bf71c30
Michael Work <miszkurkam92@gmail.com>
2022-03-23 00:32:03 +0100

    menus final

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MenuMainComponent } from './menus/menu-main/menu-main.component';
 import { MenuMealsComponent } from './menus/menu-meals/menu-meals.component';
+import { MenuMakeListComponent } from './menus/menu-make-list/menu-make-list.component';
 
 const routes: Routes = [
   {path: '', component: MenuMainComponent},
-  {path: 'meals', component: MenuMealsComponent}
+  {path: 'meals', component: MenuMealsComponent},
+  {path: 'makelist', component: MenuMakeListComponent}
 
 
 
